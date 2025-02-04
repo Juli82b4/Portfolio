7e6e8002315e31d068284be2a12e56c202771a90
Juli82b4 <157354868+Juli82b4@users.noreply.github.com>
2025-02-04 11:49:36 +0100

    add NavBar component and update index.html title; remove unused CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
-
 import "./App.css";
+import NavBar from "./Components/NavBar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <NavBar />
+    </>
+  );
 }
 
 export default App;
